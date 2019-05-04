f5e545ce4aa52e6b87e27699470343cdb6ed25d3
Gennady Verdel <godrose@gmail.com>
2019-05-04 15:35:02 +0300

    chore: updated packages #10

--- src/LogoFX.Bootstrapping.Middleware/BootstrapperExtensions.cs
+++ src/LogoFX.Bootstrapping.Middleware/BootstrapperExtensions.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Solid.Bootstrapping;
-using Solid.Extensibility;
-using Solid.Practices.Composition.Contracts;
 using Solid.Practices.IoC;
-using Solid.Practices.Middleware;
 
 namespace LogoFX.Bootstrapping
 {
@@ -84,46 +79,6 @@ namespace LogoFX.Bootstrapping
         {
             bootstrapper.Use(new RegisterBootstrapperCompositionModulesMiddleware());
             return bootstrapper;
-        }
-
-        /// <summary>
-        /// Extends the bootstrapper's functionality by using the specified collection
-        /// of dependency registrator middlewares.
-        /// </summary>
-        /// <typeparam name="TBootstrapper">The type of the bootstrapper.</typeparam>
-        /// <param name="bootstrapper">The bootstrapper.</param>
-        /// <param name="middlewares">The middlewares.</param>
-        /// <returns></returns>
-        public static TBootstrapper UseMany<TBootstrapper>(
-            this TBootstrapper bootstrapper,
-            IEnumerable<IMiddleware<IDependencyRegistrator>> middlewares) 
-            where TBootstrapper : class, IHaveRegistrator, IExtensible<TBootstrapper>
-        {
-            var bootstrapperMiddlewares =
-                middlewares.Select(
-                    t =>
-                        new UseDependencyRegistratorMiddleware<TBootstrapper>(t));
-            foreach (var bootstrapperMiddleware in bootstrapperMiddlewares)
-            {
-                bootstrapper.Use(bootstrapperMiddleware);
-            }
-            return bootstrapper;
-        }
-
-        /// <summary>
-        /// Allows using composition modules for custom dependency registrator types.
-        /// </summary>
-        /// <typeparam name="TBootstrapper">The type of the bootstrapper.</typeparam>
-        /// <typeparam name="TDependencyRegistrator">The type of the dependency registrator.</typeparam>
-        /// <param name="bootstrapper">The bootstrapper.</param>
-        /// <returns></returns>
-        public static TBootstrapper UseCompositionModules<TBootstrapper, TDependencyRegistrator>(
-            this TBootstrapper bootstrapper)
-            where TBootstrapper : class, IExtensible<TBootstrapper>,
-            IHaveRegistrator<TDependencyRegistrator>, ICompositionModulesProvider
-            where TDependencyRegistrator : class
-        {
-            return bootstrapper.Use(new RegisterCustomCompositionModulesMiddleware<TBootstrapper, TDependencyRegistrator>());
-        }
+        }        
     }
 }
